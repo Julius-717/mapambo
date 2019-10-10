24c695d659441bfbb1ec023a46279a16a1a2483f
Wangari77 <phyllismwangi7@gmail.com>
2019-10-10 21:54:52 +0300

    Added more styles to my work

--- js/dashboard.js
+++ js/dashboard.js
@@ -5,32 +5,32 @@ $(document).ready(function () {
                 var lname = document.getElementById("lname").value;
                 var pnumber = document.getElementById("pnumber").value;
                 var email = document.getElementById("email").value;
-                if (fname == "Kash" && lname == "Peach" && pnumber == "123" && email == "kashpeach@gmail.com") 
-                if (fname == "Phyllis" && lname == "Wangari" && pnumber == "123" && email == "phylliswangari@gmail.com")
-                if (fname == "Wanjiku" && lname == "Karanja" && pnumber == "123" && email == "wanjikukaranja@gmail.com")
-                if (fname == "Brian" && lname == "Mwenda" && pnumber == "123" && email == "brianmwenda@gmail.com")
-                if (fname == "Peter" && lname == "Murutu" && pnumber == "123" && email == "petermurutu@gmail.com"){
-                        // window.location.href = "index.html";
-                        return false;
-                        $('.content').hide();
-                        $('.message').show();
-                }
-         alert("Welcome to your profile" + " " +  fname + " " + lname +" " + "your number is" + " "+ pnumber + "your email is" +" " + email  );
+                if (fname == "Kash" && lname == "Peach" && pnumber == "123" && email == "kashpeach@gmail.com")
+                        if (fname == "Phyllis" && lname == "Wangari" && pnumber == "123" && email == "phylliswangari@gmail.com")
+                                if (fname == "Wanjiku" && lname == "Karanja" && pnumber == "123" && email == "wanjikukaranja@gmail.com")
+                                        if (fname == "Brian" && lname == "Mwenda" && pnumber == "123" && email == "brianmwenda@gmail.com")
+                                                if (fname == "Peter" && lname == "Murutu" && pnumber == "123" && email == "petermurutu@gmail.com") {
+                                                        // window.location.href = "index.html";
+                                                        return false;
+                                                        $('.content').hide();
+                                                        $('.message').show();
+                                                }
+                alert("Welcome to your profile" + " " + fname + " " + lname + " " + "your number is" + " " + pnumber + "your email is" + " " + email);
         });
 
         // else {
-        //                 $('.message').show();
-        //                 event.preventDefault()
-        //         }
-                
+        //         $('.message').show();
+        //         event.preventDefault()
+        // }
+
         // else {
-        //                 //   alert("Login failed! enter right  username and password.")var stylistDashboard = 'Dashboard';
+        //         //   alert("Login failed! enter right  username and password.")var stylistDashboard = 'Dashboard';
 
         //         localStorage.setItem('stylistDashboard', stylistDashboard);
 
 
-        //                   $('.message').show();
-        //                   event.preventDefault();
-        //                 }
-        });
+        //         $('.message').show();
+        //         event.preventDefault();
+        // }
+});
 
